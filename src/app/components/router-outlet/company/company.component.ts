2eb698749f14c367048518612644cc6912ca9888
Tornike Vakhtangidze <tornikevakhtangidze@gmail.com>
2024-02-04 22:37:26 +0400

    did contact and company

--- src/app/components/router-outlet/company/company.component.ts
+++ src/app/components/router-outlet/company/company.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './company.component.css'
 })
 export class CompanyComponent {
+  photos: string[] = ['achiev1.jpg', 'achiev2.jpg', 'achiev3.jpg',
+    'achiev4.jpg', 'achiev5.jpg', 'achiev6.jpg']
 
 }
